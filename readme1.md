bd00a29004d12607b144cf8c71e7c1947239644b
Bang <nnb24.112003@gmail.com>
2026-08-18 15:40:47 +0700

    feat(users): handle user hash password

---
+++ readme1.md
@@ -0,0 +1,9 @@
+# Nestjs
+ npx mikro-orm migration:create
+ npx mikro-orm migration:up  
+
+
+// cai thu vien hash password 
+
+npm i --save-exact bcryptjs@2.4.3
+yarn add bcryptjs@2.4.3
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,18 +4,25 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { User } from './entitys/user.entity';
 import { EntityRepository } from '@mikro-orm/postgresql';
-
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: EntityRepository<User>,
   ) {}
+
+  getHashPassword = (password: string) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+  };
   // create(createUserDto: CreateUserDto) {
   async create(email: string, password: string, name: string) {
+    const hashPassword = this.getHashPassword(password);
     const user = this.usersRepository.create({
       email,
-      password,
+      password: hashPassword,
       name,
     });
     await this.usersRepository.getEntityManager().flush();
